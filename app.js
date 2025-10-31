02251f6ea4968d1b09e9e41c4cc9fcf1e8e35f13
AsmamawDinku <adinku@byupathway.edu>
2025-10-31 00:30:12 -0400

    Initial commit: CSE Motors website with professional design, responsive layout, and EJS templates

---
+++ app.js
@@ -0,0 +1,32 @@
+﻿const express = require('express');
+const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+// Set EJS as templating engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Use EJS layouts
+app.use(expressLayouts);
+app.set('layout', 'layouts/layout');
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: 'CSE Motors - Home',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+// Start server
+const PORT = process.env.PORT || 5500;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
